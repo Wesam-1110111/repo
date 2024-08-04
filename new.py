61e5adadd36657d041d0efdcd2e3208642b6b5e8
Wesam <mrw202065@gmail.com>
2024-08-04 23:38:24 +0300

    fix the bugs

--- new.py
+++ new.py
@@ -2,18 +2,18 @@ import tkinter as tk
 from random import randint
 
 def rand_n():
-	return randint(10)
+	return randint(0, 10)
 
 
 print(f'this is a random number from 0 - 10: {rand_n()}')
 
 
 # setups
-root = tk.Tk(value=rand_n())
+root = tk.Tk()
 root.title('Window')
 root.geometry('400x300+700+50')
 
-randvar = tk.IntVar()
+randvar = tk.IntVar(value=rand_n())
 
 tk.Label(root, text='some text', textvariable=randvar).pack()
 
